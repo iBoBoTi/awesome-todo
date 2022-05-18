e8b211d05f4efc521898964398744bca2fbc0604
iBoBoTi <iroegbucalistus@gmail.com>
2022-05-18 18:03:07 +0100

    doc: add the description of the todo root command

--- cmd/root.go
+++ cmd/root.go
@@ -10,18 +10,15 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "awesome-todo",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Awesome Todo CLI keeps track of your todo list",
+	Long: `Awesome ToDo CLI carries out the following operations:
+			· List: list out all tasks that are not done
+			· Delete: delete tasks that have been done
+			· Add: add task to the list of tasks
+			· Complete: would mark a task as done`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -47,5 +44,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-

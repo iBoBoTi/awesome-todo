bedf6e53dcef2a9858fc3b21f966d669cf0af0df
iBoBoTi <iroegbucalistus@gmail.com>
2022-05-19 09:38:15 +0100

    style: remove unneccessary comments

--- cmd/complete.go
+++ cmd/complete.go
@@ -39,14 +39,4 @@ var CompleteCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(CompleteCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// todoCompleteCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// todoCompleteCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
